67da81784e8710e29021f872eced422e16bd46a4
till straumann <till.straumann@alumni.tu-berlin.de>
2020-11-10 09:59:35 +0100

    updated psi-file-list.tcl

--- hdl/psi-file-list.tcl
+++ hdl/psi-file-list.tcl
@@ -1,26 +1,16 @@
-set led_strip_i2c_fw_files { \
-  "ioxos_mpc_master_i2c_ctl_pkg.vhd" \
-  "MpcI2cSequencerPkg.vhd" \
+set pulseid_extractor_fw_files { \
   "Evr320StreamPkg.vhd" \
-  "LedStripWrapperPkg.vhd" \
-  "InpDebouncer.vhd" \
-  "ioxos_mpc_master_i2c_ctl.vhd" \
-  "MpcI2cSequencer.vhd" \
-  "LedStripController.vhd" \
   "PulseidExtractor.vhd" \
-  "LedStripTcsrWrapper.vhd" \
-  "LedStripTmemWrapper.vhd" \
   "PulseidAtomic.vhd" \
   "PulseidAtomicTmem.vhd" \
 }
 
-set led_strip_i2c_location "[file dirname [info script]]"
+set pulseid_extractor_location "[file dirname [info script]]"
 
-proc led_strip_i2c_add_srcs { pre } {
-  global led_strip_i2c_fw_files
-  global led_strip_i2c_location
-  set led_strip_i2ch "[file dirname [info script]]"
-  foreach f $led_strip_i2c_fw_files {
-    xfile add "$pre$led_strip_i2c_location/$f"
+proc pulseid_extractor_add_srcs { pre } {
+  global pulseid_extractor_fw_files
+  global pulseid_extractor_location
+  foreach f $pulseid_extractor_fw_files {
+    xfile add "$pre$pulseid_extractor_location/$f"
   }
 }
